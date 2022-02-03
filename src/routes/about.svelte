e8598997d42e0246b8d965dcb12ed59e86ff2eae
Zachary Burke <zachary.burke@radixeng.com>
2022-02-03 12:45:31 -0600

    05 Pages, Routing and Layouts in SvelteKit

---
+++ src/routes/about.svelte
@@ -0,0 +1,4 @@
+<h1>About Us</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere accusamus quas aliquam aut, non quisquam provident laborum possimus sunt ab. Veniam beatae incidunt quidem molestiae explicabo rem fugiat laboriosam a.
+</p>
