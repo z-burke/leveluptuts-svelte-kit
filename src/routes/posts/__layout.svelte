536ba35a95ef76b4b1cbf84646ee82172e6c644a
Zachary Burke <zachary.burke@radixeng.com>
2022-02-04 15:30:26 -0600

    10 Nested Layouts

---
+++ src/routes/posts/__layout.svelte
@@ -0,0 +1,12 @@
+<div>
+    <slot />
+    <aside>
+        <h5>Archive</h5>
+        <ul>
+            <li><a href="/posts">Home</a></li>
+            <li><a href="/posts/about">About</a></li>
+            <li><a href="">Three</a></li>
+            <li><a href="">Four</a></li>
+        </ul>
+    </aside>
+</div>
