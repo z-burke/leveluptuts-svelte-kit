1c12f1c0a9826874a8aef3174851e244e6b95087
Zachary Burke <zachary.burke@radixeng.com>
2022-02-06 01:35:30 -0600

    21 Static Site Generation and Adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-auto';
-import svelteProcess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [mdsvex(mdsvexConfig), sveltePreprocess()],
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: [svelteProcess(), mdsvex(mdsvexConfig)]
 };
 
 export default config;
