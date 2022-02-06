8bd00d353583c76b7e441c719998441e384bdd0f
Zachary Burke <zachary.burke@radixeng.com>
2022-02-05 20:22:04 -0600

    14 Markdown with MDSVEX

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,28 +1,10 @@
 <script context="module">
-// export interface LoadInput<Params extends Record<string, string> = Record<string, string>> {
-// 	url: URL;
-// 	params: Params;
-// 	props: Record<string, any>;
-// 	fetch(info: RequestInfo, init?: RequestInit): Promise<Response>;
-// 	session: App.Session;
-// 	stuff: Partial<App.Stuff>;
-// }
-
-// export interface LoadOutput<Props extends Record<string, any> = Record<string, any>> {
-// 	status?: number;
-// 	error?: string | Error;
-// 	redirect?: string;
-// 	props?: Props;
-// 	stuff?: Partial<App.Stuff>;
-// 	maxage?: number;
-// }
-
-export async function load({ params }) {
-    const post = {
-        title: params.slug,
-        date: new Date(),
-        body: 'lorem ipsum'
-    };
+    export async function load({ params }) {
+        const post = {
+            title: params.slug,
+            date: new Date(),
+            body: 'lorem ipsum'
+        };
         return {
             props: {
                 post
@@ -32,7 +14,11 @@ export async function load({ params }) {
 </script>
 
 <script>
+    import Hello from '../../posts/hello.md';
+
     export let post;
 </script>
+
+<Hello />
 
 <h3>{post.title}</h3>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-auto';
-import svelteProcess from 'svelte-preprocess'
+import svelteProcess from 'svelte-preprocess';
 
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: svelteProcess()
+	preprocess: [svelteProcess(), mdsvex(mdsvexConfig)]
 };
 
 export default config;
