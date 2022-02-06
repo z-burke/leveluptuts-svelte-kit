a4e2322855b1bc179dc780e0c23a82ac69cc1987
Zachary Burke <zachary.burke@radixeng.com>
2022-02-05 22:16:22 -0600

    17 Glob Imports

---
+++ src/routes/posts/__layout.svelte
@@ -0,0 +1,32 @@
+<script context="module">
+    export async function load() {
+        const posts = import.meta.globEager('../../posts/*.md');
+        // console.log('posts', posts);
+        const postsList = Object.values(posts);
+        const postsMeta = postsList.map((post) => {
+            return post.metadata;
+        });
+        console.log('postsMeta', postsMeta);
+        return {
+            props: {
+                posts: postsMeta
+            }
+        };
+    }
+</script>
+
+<script>
+    export let posts;
+</script>
+
+<div>
+    <slot />
+    <aside>
+        <h5>Archive</h5>
+        <ul>
+            {#each posts as post}
+                <li><a href={post.slug}>{post.title}</a></li>
+            {/each}
+        </ul>
+    </aside>
+</div>
